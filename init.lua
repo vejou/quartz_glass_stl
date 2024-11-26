7003bc2dd298b0e05f954b55bc748ca36bd45996
inckij <65826623+vejou@users.noreply.github.com>
2024-11-25 21:03:06 -0500

    Less crash again & reupload

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local string m = "quartz_glass_stl:"
 
 
 
-	local function makeglassblock(modpathname, blockname) {
+	local function makeglassblock(modpathname, blockname) 
 		core.register_node(m .. modpathname, {
 			description = blockname,
 			tiles = {modpathname .. ".png"},
@@ -12,7 +12,7 @@ local string m = "quartz_glass_stl:"
 			paramtype = "light",
 			sunlight_propagates = true,
 		})
-	}
+	end
 	makeglassblock("aventurine_glass", "Aventurine Tinted Glass")
 	makeglassblock("citrine_glass", "Citrine Tinted Glass")
 	makeglassblock("rose_glass", "Rose Quartz Tinted Glass")
