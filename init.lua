edea3bdef4c33bf57d675b7f348a1084a2c468ac
inckij <65826623+vejou@users.noreply.github.com>
2024-11-25 20:38:14 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -4,26 +4,36 @@ local string m = "quartz_glass_stl:"
 	description = "Aventurine Tinted Glass",
 	tiles = {"aventurine_glass.png"},
 	groups = {cracky = 1, spaceship = 1},
+	use_texture_alpha = "blend", -- thanks theidealist and minetest game
+	drawtype = "glasslike",
 	})
 	core.register_node(m .. "citrine_glass", {
 	description = "Citrine Tinted Glass",
 	tiles = {"citrine_glass.png"},
 	groups = {cracky = 1, spaceship = 1},
+	use_texture_alpha = "blend", -- thanks theidealist
+	drawtype = "glasslike",
 	})
 	core.register_node(m .. "rose_glass", {
 	description = "Rose Quartz Tinted Glass",
 	tiles = {"rose_glass.png"},
 	groups = {cracky = 1, spaceship = 1},
+	use_texture_alpha = "blend", -- thanks theidealist
+	drawtype = "glasslike",
 	})
 	core.register_node(m .. "clearquartz_glass", {
 	description = "Clear-Quartz Tinted Glass",
 	tiles = {"clearquartz_glass.png"},
 	groups = {cracky = 1, spaceship = 1},
+	use_texture_alpha = "blend", -- thanks theidealist
+	drawtype = "glasslike",
 	})
 	core.register_node(m .. "amethyst_glass", {
 	description = "Amethyst Tinted Glass",
 	tiles = {"amethyst_glass.png"},
 	groups = {cracky = 1, spaceship = 1},
+	use_texture_alpha = "blend", -- thanks theidealist
+	drawtype = "glasslike",
 	})
 
 local function defglass(glassinput, glassoutput)
@@ -37,8 +47,8 @@ local function defglass(glassinput, glassoutput)
 	})
 end
 
-defglass("stl_core:quartz1", m .. "amethyst_glass")
-defglass("stl_core:quartz2", m .. "citrine_glass")
-defglass("stl_core:quartz3", m .. "clearquartz_glass")
-defglass("stl_core:quartz4", m .. "rose_glass")
-defglass("stl_core:quartz5", m .. "aventurine_glass")
+defglass("stl_core:quartz1", m .. "amethyst_glass 8")
+defglass("stl_core:quartz2", m .. "citrine_glass 8")
+defglass("stl_core:quartz3", m .. "clearquartz_glass 8")
+defglass("stl_core:quartz4", m .. "rose_glass 8")
+defglass("stl_core:quartz5", m .. "aventurine_glass 8")
