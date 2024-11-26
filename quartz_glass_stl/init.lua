397d50358dc5d3f8ef43b06b5ac6cc059cef8b2b
inckij <65826623+vejou@users.noreply.github.com>
2024-11-25 19:51:42 -0500

    Add files via upload

---
+++ quartz_glass_stl/init.lua
@@ -0,0 +1,44 @@
+local string m = "quartz_glass_stl:"
+
+	core.register_node(m .. "aventurine_glass", {
+	description = "Aventurine Tinted Glass",
+	tiles = {"aventurine_glass.png"},
+	groups = {cracky = 1, spaceship = 1},
+	})
+	core.register_node(m .. "citrine_glass", {
+	description = "Citrine Tinted Glass",
+	tiles = {"citrine_glass.png"},
+	groups = {cracky = 1, spaceship = 1},
+	})
+	core.register_node(m .. "rose_glass", {
+	description = "Rose Quartz Tinted Glass",
+	tiles = {"rose_glass.png"},
+	groups = {cracky = 1, spaceship = 1},
+	})
+	core.register_node(m .. "clearquartz_glass", {
+	description = "Clear-Quartz Tinted Glass",
+	tiles = {"clearquartz_glass.png"},
+	groups = {cracky = 1, spaceship = 1},
+	})
+	core.register_node(m .. "amethyst_glass", {
+	description = "Amethyst Tinted Glass",
+	tiles = {"amethyst_glass.png"},
+	groups = {cracky = 1, spaceship = 1},
+	})
+
+local function defglass(glassinput, glassoutput) {
+	core.register_craft({
+		output = glassoutput,
+		recipe = {
+		{glassinput, glassinput, glassinput},
+		{glassinput, "stl_decor:glass", glassinput},
+		{glassinput, glassinput, glassinput}
+		}
+	})
+}
+
+defglass("stl_core:quartz1", m .. "amethyst_glass")
+defglass("stl_core:quartz2", m .. "citrine_glass")
+defglass("stl_core:quartz3", m .. "clearquartz_glass")
+defglass("stl_core:quartz4", m .. "rose_glass")
+defglass("stl_core:quartz5", m .. "aventurine_glass")
