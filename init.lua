6cf1a5d0f4c5675f8bfa3b500e205683544a9eb1
inckij <65826623+vejou@users.noreply.github.com>
2024-11-25 21:02:13 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,40 +1,25 @@
 local string m = "quartz_glass_stl:"
 
-	core.register_node(m .. "aventurine_glass", {
-	description = "Aventurine Tinted Glass",
-	tiles = {"aventurine_glass.png"},
-	groups = {cracky = 1, spaceship = 1},
-	use_texture_alpha = "blend", -- thanks theidealist and minetest game
-	drawtype = "glasslike",
-	})
-	core.register_node(m .. "citrine_glass", {
-	description = "Citrine Tinted Glass",
-	tiles = {"citrine_glass.png"},
-	groups = {cracky = 1, spaceship = 1},
-	use_texture_alpha = "blend", -- thanks theidealist
-	drawtype = "glasslike",
-	})
-	core.register_node(m .. "rose_glass", {
-	description = "Rose Quartz Tinted Glass",
-	tiles = {"rose_glass.png"},
-	groups = {cracky = 1, spaceship = 1},
-	use_texture_alpha = "blend", -- thanks theidealist
-	drawtype = "glasslike",
-	})
-	core.register_node(m .. "clearquartz_glass", {
-	description = "Clear-Quartz Tinted Glass",
-	tiles = {"clearquartz_glass.png"},
-	groups = {cracky = 1, spaceship = 1},
-	use_texture_alpha = "blend", -- thanks theidealist
-	drawtype = "glasslike",
-	})
-	core.register_node(m .. "amethyst_glass", {
-	description = "Amethyst Tinted Glass",
-	tiles = {"amethyst_glass.png"},
-	groups = {cracky = 1, spaceship = 1},
-	use_texture_alpha = "blend", -- thanks theidealist
-	drawtype = "glasslike",
-	})
+
+
+	local function makeglassblock(modpathname, blockname) {
+		core.register_node(m .. modpathname, {
+			description = blockname,
+			tiles = {modpathname .. ".png"},
+			groups = {cracky = 1, spaceship = 1},
+			use_texture_alpha = "blend", -- thanks theidealist and minetest game
+			drawtype = "glasslike",
+			paramtype = "light",
+			sunlight_propagates = true,
+		})
+	}
+	makeglassblock("aventurine_glass", "Aventurine Tinted Glass")
+	makeglassblock("citrine_glass", "Citrine Tinted Glass")
+	makeglassblock("rose_glass", "Rose Quartz Tinted Glass")
+	makeglassblock("clearquartz_glass", "Clear-Quartz Tinted Glass")
+	makeglassblock("amethyst_glass", "Amethyst Tinted Glass")
+	
+	makeglassblock("blue_quartz_glass", "Blue Quartz Glass")
 
 local function defglass(glassinput, glassoutput)
 	core.register_craft({
@@ -52,3 +37,4 @@ defglass("stl_core:quartz2", m .. "citrine_glass 8")
 defglass("stl_core:quartz3", m .. "clearquartz_glass 8")
 defglass("stl_core:quartz4", m .. "rose_glass 8")
 defglass("stl_core:quartz5", m .. "aventurine_glass 8")
+defglass("stl_decor:glass", m .. "blue_quartz_glass 9")
